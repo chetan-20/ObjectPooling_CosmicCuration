de711b0f45dcb43dac8b3960eeac83f4e49c8fe5
Chetan Rautela <chetanrautela20@gmail.com>
2024-05-28 15:11:00 +0530

    created bullet bool

--- Assets/Scripts/Bullets/BulletPool.cs
+++ Assets/Scripts/Bullets/BulletPool.cs
@@ -1,6 +1,8 @@
 
 using System.Collections.Generic;
+using Unity.Services.Analytics;
 using UnityEngine;
+using static CosmicCuration.Bullets.BulletPool;
 
 namespace CosmicCuration.Bullets
 { 
@@ -13,6 +15,27 @@ namespace CosmicCuration.Bullets
         {
             bulletView = bView;
             bulletScriptableObject = bScriptableObject;
+        }
+        public BulletController GetBullet()
+        {
+            if (pooledBullets.Count > 0)
+            {
+                PooledBullet pooledBullet = pooledBullets.Find(item => !item.isused);
+                if (pooledBullet != null)
+                {                   
+                    pooledBullet.isused = true;                  
+                    return pooledBullet.bullet;
+                }
+            }
+            return CreateNewPooledBullet();
+        }
+        private BulletController CreateNewPooledBullet()
+        {
+            PooledBullet poolBullet = new PooledBullet();
+            poolBullet.bullet = new BulletController(bulletView,bulletScriptableObject);
+            poolBullet.isused = true;
+            pooledBullets.Add(poolBullet);
+            return poolBullet.bullet;
         }
         public class PooledBullet
         {
