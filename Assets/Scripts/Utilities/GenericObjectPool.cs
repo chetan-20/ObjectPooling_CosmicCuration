5fcd42ce2eb5a2c05b256b1c52c5296eead089a1
Chetan Rautela <chetanrautela20@gmail.com>
2024-05-30 15:07:13 +0530

    Update GenericObjectPool.cs

--- Assets/Scripts/Utilities/GenericObjectPool.cs
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class GenericObjectPool : MonoBehaviour
+using System.Collections.Generic;
+using static CosmicCuration.Bullets.BulletPool;
+
+
+public class GenericObjectPool<T> where T : class
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<PooledItem<T>> pool = new List<PooledItem<T>>();
+
+    protected T GetItem()
     {
-        
+        if (pool.Count > 0)
+        {
+            PooledItem<T> item = pool.Find(item => !item.isUsed);
+            if (item != null)
+            {
+                item.isUsed = true;
+                return item.Item;
+            }
+        }
+        return CreateNewPoolItem();
     }
 
-    // Update is called once per frame
-    void Update()
+    private T CreateNewPoolItem()
     {
-        
+        PooledItem<T> newItem = new PooledItem<T>();
+        newItem.Item = CreateItemT();
+        newItem.isUsed = true;
+        pool.Add(newItem);
+        return newItem.Item;
+    }
+    public void ReturnItem(T item)
+    {
+        PooledItem<T> poolItem = pool.Find(i => i.Item.Equals(item));
+        poolItem.isUsed = false;
+    }
+    protected virtual T CreateItemT()
+    {
+       throw new System.Exception("Not Implemented in Child Class");
+    }
+    public class PooledItem<T>
+    {
+        public T Item;
+        public bool isUsed;
     }
 }
