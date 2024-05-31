77e03591e593fb2f4baa0c3fefe6d74659f576a3
Chetan Rautela <chetanrautela20@gmail.com>
2024-05-31 17:09:39 +0530

    implemented returning of powerup to the pool

--- Assets/Scripts/PowerUps/PowerUpPool.cs
+++ Assets/Scripts/PowerUps/PowerUpPool.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+
+using CosmicCuration.Utilities;
+using System;
 using UnityEngine;
 
-public class PowerUpPool : MonoBehaviour
+namespace CosmicCuration.PowerUps
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PowerUpPool : GenericObjectPool<PowerUpController>
     {
-        
-    }
+        private PowerUpData powerUpData;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public PowerUpController GetPowerUp<T>(PowerUpData pwrUpData) where T : PowerUpController
+        {
+            this.powerUpData = pwrUpData;
+            return GetItem<T>();
+        }
+        protected override PowerUpController CreateItem<T>()
+        {
+           if(typeof(T)==typeof(Shield))
+            {
+                return new Shield(powerUpData);
+            }
+           else if (typeof(T) == typeof(DoubleTurret))
+            {
+                return new DoubleTurret(powerUpData);
+            }
+            else if (typeof(T) == typeof(RapidFire))
+            {
+                return new RapidFire(powerUpData);
+            }
+            else
+            {
+                throw new NotSupportedException("PowerUp Not Supported");
+            }
+
+        }
     }
 }
